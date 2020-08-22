5093e1d53ca347fcfc58c5e2ace26baeef28ac9d
ornwipa <fahornwipa@gmail.com>
2020-08-22 15:00:48 -0600

    controller methods and views for 'edit' page

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -43,5 +43,58 @@ namespace MvcMovie.Controllers
         {
             return View(await _context.Movie.ToListAsync());
         }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    // if (!MovieExists(movie.Id))
+                    // {
+                    //     return NotFound();
+                    // }
+                    // else
+                    // {
+                    //     throw;
+                    // }
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
     }
 }
